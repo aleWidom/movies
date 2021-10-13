136414b9a6914f2ba8856d275b6097f66ca1983e
Alejandro Widomlanski <widomlanski@hotmail.com>
2021-10-12 21:28:10 -0300

    movies-useEffect

--- src/components/ListadoPeliculas.js
+++ src/components/ListadoPeliculas.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from 'react'; 
+import { useEffect, useState } from 'react';
 import movies from '../data/data.json'
 import Pelicula from './Pelicula'
 import styles from './ListadoPeliculas.module.css'
 
 
+
 const ListadoPeliculas = () => {
 
-  console.log(styles)
+  const [state, setstate] = useState([])
+
+  useEffect(() => {
+     fetch('https://api.themoviedb.org/3/discover/movie', {
+       headers:  {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs",
+          "Content-Type": "application/json;charset=utf-8",
+      }
+     })
+     .then((res)=> res.json())
+     .then((data) => setstate(data.results))
+  }, [])
+  
+
+  console.log(state)
+
   return (
         <ul className={styles.listadoPeliculas}> 
-          {movies.map((movie)=>(
+          {state.map((movie)=>(
             <Pelicula key={movie.id} movie={movie}/>
           ))}
         </ul>
